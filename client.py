9cd2820404726f67b96b35c71475b5ed9c88f384
FJJI <fjjimenez@miuandes.cl>
2019-11-27 23:42:47 -0300

    Cliente

--- client.py
+++ client.py
@@ -9,9 +9,52 @@ host_IP = "127.0.0.8"
 host_port = 6000
 BUFFER_SIZE = 4096
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cmd = ""
+connected = 0
+
+def Main():
+	host_path = Get_Socket_Path()
+
+	host_IP = "127.0.0.8"
+	host_port = 6000
+	BUFFER_SIZE = 4096
+
+	cmd = ""
+	connected = 0
+	while (( cmd != "quit" )or(cmd != "disconnect")):
+		if (connected == 1):
+			PrintInfo("Estado: Conectado al Servidor\n")
+		else:
+			PrintInfo("Estado: Desconectado\n")
+		if(cmd == "quit"):
+			connection.close()
+			connected = 0
+			exit(0)
+		cmd = input(">")
+		if ( cmd =="connect"):
+			connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			# connect to server on local computer
+			connection.connect((host_IP, host_port))
+			connected = 1
+			continue
+		elif( cmd == "disconnect"):
+			msg = "out" #poner el que corresponde
+			connection.send(msg.encode('utf-8'))
+			connection.close()
+			PrintInfo("Usted se ha desconectado del servidor\n")
+			connected = 0
+			continue
+	try:
+		s.close()
+	finally:
+		pass
+
+
+if __name__ == '__main__':
+    Main()
 
 def TEST():
+	connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print("Connecting to server in IP address {} in port {}...".format(host_IP, host_port))
 	connection.connect((host_IP, host_port))
 	
